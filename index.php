8237629b8f4389cfbe16a3e25209b7e8268c244d
LuisDumbo <luisdumbo12@gmail.com>
2022-12-02 16:10:31 +0100

    Upload  file

--- index.php
+++ index.php
@@ -41,6 +41,65 @@ Route::middleware(AdminMiddleware::class)->prefix("api/")->group(function () {
     Route::get("/listar_exame", [ExameController::class, 'listar_exame']);
     Route::get("/exame", [ExameController::class, 'um_exame']);
     Route::post("/editar_exame", [ExameController::class, 'editar_exame']);
+
+    Route::post("/arquivo", function () {
+
+
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+    
+        header('Content-type: application/json');
+        header("Acess-Control-Allow-Methods: POST");
+        header("Acess-Control-Allow-Headers: Acess-Control-Allow-Headers,Content-Type,Acess-Control-Allow-Methods, Authorization");
+    
+    
+    
+        $data = json_decode(file_get_contents("php://input"), true); // collect input parameters and convert into readable format
+    
+        $fileName  =  $_FILES['sendimage']['name'];
+        $tempPath  =  $_FILES['sendimage']['tmp_name'];
+        $fileSize  =  $_FILES['sendimage']['size'];
+    
+        if (empty($fileName)) {
+            $errorMSG = json_encode(array("message" => "please select image", "status" => false));
+            echo $errorMSG;
+        } else {
+            $upload_path = 'upload/'; // set upload folder path 
+    
+            $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION)); // get image extension
+    
+            // valid image extensions
+            $valid_extensions = array('jpeg', 'jpg', 'png', 'gif');
+    
+            // allow valid image file formats
+            if (in_array($fileExt, $valid_extensions)) {
+                //check file not exist our upload folder path
+                if (!file_exists($upload_path . $fileName)) {
+                    // check file size '5MB'
+                    if ($fileSize < 5000000) {
+                        move_uploaded_file($tempPath, $upload_path . $fileName); // move file from system temporary path to our upload folder path 
+                        $errorMSG = json_encode(array("message" => $fileName, "status" => true));
+                        echo $errorMSG;
+                    } else {
+                        $errorMSG = json_encode(array("message" => "Sorry, your file is too large, please upload 5 MB size", "status" => false));
+                        echo $errorMSG;
+                    }
+                } else {
+                    $errorMSG = json_encode(array("message" => "Sorry, file already exists check upload folder", "status" => false));
+                    echo $errorMSG;
+                }
+            } else {
+                $errorMSG = json_encode(array("message" => "Sorry, only JPG, JPEG, PNG & GIF files are allowed", "status" => false));
+                echo $errorMSG;
+            }
+        }
+    
+        // if no error caused, continue ....
+        if (!isset($errorMSG)) {
+    
+            echo json_encode(array("message" => "Image Uploaded Successfully", "status" => true));
+        }
+    });
 });
 
 
@@ -61,6 +120,8 @@ Route::middleware(UserMiddleware::class)->group(function () {
 Route::get("/Registar", [RegistarUserController::class, 'regitar'])->name("Registar");
 Route::post("/Registar", [RegistarUserController::class, 'cadastrar'])->name("registar");
 Route::get("/", [UserController::class, 'home'])->name('home');
+
+
 
 
 Route::get("/not", function () {
